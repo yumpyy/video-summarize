3a25f9be9c2d47a6255360d1b4f8a189854106f2
yumpyy <121102787+yumpyy@users.noreply.github.com>
2024-08-02 22:20:52 +0530

    feat: add cli options
    update .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import argparse
+
 import utils
+
+parser = argparse.ArgumentParser(prog='video-summarize', description='summarize youtube videos using llm')
+
+parser.add_argument('link', help='youtube link for summarizing')
+parser.add_argument('-o', '--output', help='write output to a file')
+
+args = parser.parse_args()
 
 def main(url):
     utils.extract_transcript(url)
@@ -7,7 +16,13 @@ def main(url):
     with open("./transcript.en.srt") as f:
         data = f.read()
         filtered_data = utils.filter_transcript(data)
+
+        if args.output:
+            with open(args.output, "w") as f:
+                f.write(filtered_data)
+                print(f"Writing output to \'{args.output}\'")
+                exit(1)
         print(filtered_data)
 
 if __name__ == "__main__":
-    main("https://youtu.be/4u5x9e226i4")
+    main(args.link)
