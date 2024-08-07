a2fa968c087a50908dff5143a2a733ac199528a5
yumpyy <121102787+yumpyy@users.noreply.github.com>
2024-08-07 14:02:27 +0530

    fix: remove duplicate sentence(s) from transcript

--- utils.py
+++ utils.py
@@ -7,14 +7,17 @@ from ffmpeg import FFmpeg
 def filter_transcript(data):
     pattern_list = [
         "[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}",  # for matching 00:00:00,000 --> 00:00:39,410
-        "[0-9]{1,10}",                                                                  # for matching lines starting with numbers
+        "^[0-9]{1,10}",                                                                  # for matching lines starting with numbers
     ]
-    data = re.sub("|".join(pattern_list), "", data)
 
-    filtered_data = []
+    filtered_data = [""]
     for line in data.split('\n'):
+        line = re.sub("|".join(pattern_list), "", line)
         if line.strip():
-            filtered_data.append(line)
+            if filtered_data[-1] == line:
+                continue
+            else:
+                filtered_data.append(line)
     
     filtered_data = " ".join(filtered_data)
     return filtered_data
